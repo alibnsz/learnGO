ae9b45c2626a44dae6a7bb6e59d37f4eb2d1060e
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2025-07-11 10:48:40 +0300

    feat: Implement ProductRepository with GetAllProducts method.

--- persistence/product_repository.go
+++ persistence/product_repository.go
@@ -1,1 +1,46 @@
 package persistence
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/labstack/gommon/log"
+	"learnGo/domain"
+)
+
+type IProductRepository interface {
+	GetAllProducts() []domain.Product
+}
+
+type ProductRepository struct {
+	dbPool *pgxpool.Pool
+}
+
+func (productRepository *ProductRepository) GetAllProducts() []domain.Product {
+
+	ctx := context.Background()
+	productRows, err := productRepository.dbPool.Query(ctx, "select * from products")
+
+	if err != nil {
+		log.Errorf("Error while getting product rows: %v", err)
+		return []domain.Product{}
+	}
+
+	var products = []domain.Product{}
+	var id int64
+	var name string
+	var price float32
+	var discount float32
+	var store string
+
+	for productRows.Next() {
+		productRows.Scan(&id, &name, &price, &discount, &store)
+		products = append(products, domain.Product{
+			Id:       id,
+			Name:     name,
+			Price:    price,
+			Discount: discount,
+			Store:    store,
+		})
+	}
+	return products
+}
